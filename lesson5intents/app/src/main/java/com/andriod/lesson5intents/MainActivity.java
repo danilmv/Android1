0e755c786da794080af29432e1220cdc197d4b81
danilmv <43599357+danilmv@users.noreply.github.com>
2021-04-30 16:41:44 +0300

    lesson5.Intents

--- lesson5intents/app/src/main/java/com/andriod/lesson5intents/MainActivity.java
+++ lesson5intents/app/src/main/java/com/andriod/lesson5intents/MainActivity.java
@@ -2,7 +2,11 @@ package com.andriod.lesson5intents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonCalc = findViewById(R.id.buttonCalc);
+        buttonCalc.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mycalculator://start/123/add/444"));
+            startActivity(intent);
+        });
     }
 }
